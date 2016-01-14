23ca3d9a81904c47aa5e163b2ee1b336bbca4ad1
John Malmberg <wb8tyw@qsl.net>
2016-01-13 20:46:26 -0600

    Make version string unique to GNV fork

--- vms_source/gnv_make/main.c
+++ vms_source/gnv_make/main.c
@@ -2748,7 +2748,7 @@ print_version ()
     /* Do it only once.  */
     return;
 
-  printf ("%sGNU Make version %s", precede, version_string);
+  printf ("%sGNV Fork of GNU Make version %s", precede, version_string);
   if (remote_description != 0 && *remote_description != '\0')
     printf ("-%s", remote_description);
 
@@ -2759,7 +2759,7 @@ print_version ()
 %sThis is free software; see the source for copying conditions.\n\
 %sThere is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A\n\
 %sPARTICULAR PURPOSE.\n\n\
-%sReport bugs to <bug-make@gnu.org>.\n\n"),
+%sReport bugs to https://sourceforge.net/p/gnv/bugs/\n\n"),
           precede, make_host,
 	  precede, precede, precede, precede, precede, precede);
 
